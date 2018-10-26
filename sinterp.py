7d24023d55b7dd977a29083388ade30c8464ba3d
Matt Guptil <mdguptil@gmail.com>
2018-10-26 18:02:43 -0400

    Added some exception handling and functionality to ranger function. Need to finish adding these to the other functions

--- sinterp.py
+++ sinterp.py
@@ -1,5 +1,10 @@
 from Filtering import myfilter
 import numpy as np
+
+
+#  Takes in arrays and performs linear interpolation, checks range boundaries, and outputs filtered data
+#  Can't test this as it is made up of tested functions so output will always be as expected when other functions pass
+#  As long as functions check for exceptions
 def dataCleaner(ecgd):
 
 	time = sinterp(ecgd[0])
@@ -11,19 +16,26 @@ def dataCleaner(ecgd):
 
 	time, fvolt = myfilter(ecgd)
 
-def typeCorrection(arry):
-
-	for j,each in arry:
-		
+	return time, fvolt
 
 
+#  Goes through entire array to see if there is a boundary violation, fixes those issues
+# . Inputs an array, Outputs fixed array
 def ranger(volt):
 
-	for j,each in volt:
+	if isinstance(volt, str) or isinstance(volt, int):
+		raise TypeError("ERROR: Input was not a proper iterable, ")
+
+	for j, each in enumerate(volt):
 		if each >= 300:
 			volt[j] = 299
+		if each <= -300:
+			volt[j] = -299
 	return volt
 
+
+# Performs my own custom linear interpolation on datasets when they are missing values
+# Outputs fixed array/dataset
 def sinterp(time):
 	
 	i = 0
@@ -55,8 +67,7 @@ def sinterp(time):
 
 			myint = float((time[j] -time[i-1])) / float(count)
 			k = i-1
-			#print(myint)
-			print('k=',k , '  j=',j)
+
 			while(True):
 				
 				if k >= j-1:
@@ -69,8 +80,8 @@ def sinterp(time):
 	return time
 
 
-if __name__ == "__main__":
-    myarr = np.array([-999, 1, -999, -999, 5, 7, -999, 15 , -999])
-
-    t = sinterp(myarr)
-    print(t)
+# if __name__ == "__main__":
+# 	myarr = np.array([-999, 1, -999, -999, 5, 7, -999, 15 , -999])
+#
+#  t = sinterp(myarr)
+# print(t)
