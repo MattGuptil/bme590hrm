e2dfdf922f1b6ce6e7487755466443b82668c654
Matt Guptil <mdguptil@gmail.com>
2018-10-25 22:06:53 -0400

    Finished code that will give us metrics. Next we need to create a function that will properly out metric as a JSON file. Also fixed pep8 errors

--- newindice.py
+++ newindice.py
@@ -2,20 +2,22 @@ from peakalgorithm import detect_peaks
 from Datacollector import collector
 import numpy as np
 
+
 def peakfinder():
-	
 
 	ecgd = collector()
 	time = ecgd[0]
 	vd = ecgd[1]
 	
-	myindices = detect_peaks(vd,mph = 0, show =False)
+	myindices = detect_peaks(vd, mph=0, show=False)
 
-	return(time,myindices)
+	return time, myindices
 
 
-def timefinder(mytime,myindex):
+def timefinder(mytime, myindex):
+
 	newtime = mytime[myindex]
+
 	return newtime
 
 
@@ -32,31 +34,52 @@ def avghr(dur,tarr):
 		k = 0
 		mylen = len(tarr)
 
-	while(True):
+	while():
 		if j >= mylen:
 			break
-		if tarr[j]-tarr[i] >= tint or j = mylen-1:
-			beats[k] = 1+ j-i
+		if tarr[j]-tarr[i] >= tint or j == mylen-1:
+			beats[k] = 1 + j-i
 			k = k+1
 			i = j
-			j = i +1
+			j = i + 1
 		else:
-			j = j+1
+			j = j + 1
 
+	bps = np.true_divide(beats, tint)  # might need to add beats to input here
 
-	return beats
+	bpm = bps*60
+
+	avgbpm = np.mean(bpm)
+
+	return avgbpm
+
 
 def durr(tarr):
-	f = len(tarr) -1
+	f = len(tarr) - 1
 
 	dur = tarr[f]-tarr[0]
 
 	return dur
 
 
-if __name__ == '__main__':
-	a,b = peakfinder()
+def findex(volt):
 
-	c = timefinder(a,b)
+	mmax = np.max(volt)
+	mmin = np.min(volt)
+
+	return mmax, mmin
+
+
+def numbeats(beatindex):
+
+	mynum = len(beatindex)
+
+	return mynum
+
+
+if __name__ == '__main__':
+	a, b = peakfinder()
+
+	c = timefinder(a, b)
 	mydur = durr(a)
-	avghr(mydur,c)
+	avghr(mydur, c)
