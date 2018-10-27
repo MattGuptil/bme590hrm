957fc8bb76d09a05b7b2160acec86d36a6fe1462
Matt Guptil <mdguptil@gmail.com>
2018-10-26 23:43:37 -0400

    Fixed File creation

--- makeJson.py
+++ makeJson.py
@@ -8,16 +8,17 @@ def makej(mydir, fname):
     if not isinstance(fname, str):
         raise TypeError("File Name is not a string")
 
-    fname = fname.strip('.csv')
+    fname = fname.strip('.csv').strip('\\')
     fname = fname + '.json'
     with open(fname, 'w') as fp:
+        print(fname)
         json.dump(mydir, fp)
-
+    print(json.dumps(mydir))
     return
 
 
 if __name__ == '__main__':
-    fname = 'myTEST.csv'
-    mydir = 1
+    fname = 'myTEST2.csv'
+    mydir = {'a': 1, 'b': [1, 2, 2]}
 
     makej(mydir, fname)
